38fb01cd8a25c01c6732a0502956591f9bbe9038
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-05-02 08:09:58 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
             </p>
           </div>
         </div>
-        <a
+        {/* <a
           id="resume-link-2"
           className="md:w-[160px] xs:w-[100px] h-[60px] p-9 flex mt-4 justify-between items-center  hover: duration-300 bg-[#565f69]"
           href={Resume}
@@ -49,7 +49,7 @@ const About = () => {
           >
             <BsFillPersonLinesFill size={30} /> Resume
           </div>
-        </a>
+        </a> */}
       </div>
     </div>
   );
