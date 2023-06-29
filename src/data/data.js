9b15a4e5005b51a47baf620f75dce46423ca5884
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2023-06-29 20:22:47 +0530

    update

--- src/data/data.js
+++ src/data/data.js
@@ -8,6 +8,7 @@ import thisImg from "../assets/creator.png";
 import useReducerImg from "../assets/web.png";
 import useStateImg from "../assets/backend.png";
 import CodeVortex from "../assets/CodeVortexInterview.png";
+import hrDashboard from "../assets/projects/HR Dashboard.png";
 
 export const data = [
   {
@@ -56,7 +57,7 @@ export const data = [
     ],
   },
   {
-    id: 5,
+    id: 4,
     name: "React JS Application",
     image: CodeVortex,
     pName: "Interview Simulator",
@@ -64,6 +65,21 @@ export const data = [
       "Code Vortex is an innovative self-interview prep platform harnessing the power of AI and natural language processing. It leverages OpenAI's advanced capabilities and prompt engineering techniques to replicate real interview scenarios. With personalized feedback and a comprehensive set of resources, Code Vortex empowers users to enhance their interview skills and excel in their job search.",
     github: "https://github.com/GRAviTY-GAGAN/CodeVertex-FE",
     live: "https://interview-simulator-open-ai.netlify.app/",
+    tech: [
+      { name: "#ReactJS", color: "red" },
+      { name: "#MongoDB", color: "orange" },
+      { name: "#ExpressJS", color: "green" },
+    ],
+  },
+  {
+    id: 5,
+    name: "React JS Application",
+    image: hrDashboard,
+    pName: "HR Dashboard",
+    pDesc:
+      "HR dashboard application that empowers both administrators and users to effortlessly access and manage essential HR information. From viewing shift schedules and holiday details to checking salary information, offering a comprehensive overview of crucial HR aspects. Additionally, users can raise issues related to salary or holiday matters, ensuring efficient problem resolution. Moreover, the app allows users to save and organize their daily tasks, enhancing productivity and organization.",
+    github: "https://github.com/GRAviTY-GAGAN/hr-dashboard-fe",
+    live: "https://hr-dashboard-app.netlify.app",
     tech: [
       { name: "#ReactJS", color: "red" },
       { name: "#MongoDB", color: "orange" },
